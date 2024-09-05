6fb6d82c024913c42de9281815f59f1cd2459b80
Michael Everette <michael.d.everette@gmail.com>
2024-09-04 22:27:38 -0700

    initial commit

---
+++ src/SculptureGrid.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : SculptureGrid.cpp
+// Author      : Michael Everette
+// Version     :
+// Copyright   : 
+// Description : Process image to generate grid of blocks which will make up a sculpture.
+//  Each block will be a rectangular prism of a particular length with one angled face.
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "test" << endl; // prints test message
+	return 0;
+}
